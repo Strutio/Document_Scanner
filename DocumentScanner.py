c395580a31e6a49f5e37aaea16c5cbf330f3b112
Strutio <romeoalrabi@hotmail.com>
2021-05-08 18:04:26 -0400

    Changed the device that is being used to capture from my computer, otherwise no new functions

--- DocumentScanner.py
+++ DocumentScanner.py
@@ -3,14 +3,14 @@ import cv2
 
 # Beginning by accessing the webcam from the user
 
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(1)
 
 
 # Making sure the webcam is being opened
 # raises an exception otherwise
 
 if not cam.isOpened():
-    raise IOError("Webcam failed to open")
+   raise IOError("Webcam failed to open")
 
 #todo 
   # access the screenshot the user creates, subsequently named file_screenshot_count or whatever
@@ -23,11 +23,12 @@ if not cam.isOpened():
 count = 0
 
 while True:
-
+  
     ret, frame = cam.read()
+    
     frame = cv2.resize(frame, None, fx=2.5, fy=2.5, interpolation=cv2.INTER_AREA)
  ##   cv2.imshow('Input', frame)
-
+  
     c = cv2.waitKey(1)
     if c == 27:
         break
@@ -40,27 +41,31 @@ while True:
       
         count += 1
 
-cap.release()
-cv2.destroyAllWindows()
-
 # moving on to detecting  edges in the image
 
 # beginning by adding grayscale to the webcam
 # using gaussian blur to remove extra noise and detail, makes it easier to find the edges
 
-frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-frame = cv2.GaussianBlur(frame, (7, 7), 1.41)
 
-    # smoothening the image to help find edges  
+    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.GaussianBlur(frame, (7, 7), 1.41)
+ 
+# smoothening the image to help find edges
 
-kernel = np.ones((15,15),np.float32)/225
-smooth = cv2.filter2D(frame, -1, kernel)
+    kernel = np.ones((15,15),np.float32)/225
+    smooth = cv2.filter2D(frame, -1, kernel)
+
+    cv2.imshow('Original video', frame)
+
+cam.release()
+cv2.destroyAllWindows()
+
 
   #  threshold = cv2.adaptiveThreshold(smooth, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN.C, cv2.THRESH_BINARY,11,2)
 
 # displaying the current webcam feed to the monitor
 
-cv2.imshow('Original video', frame)    
+# cv2.imshow('Original video', frame)    
    # cv2.imshow('Original video', smooth)
 
 
